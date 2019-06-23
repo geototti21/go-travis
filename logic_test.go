634e70a1d269d2283967ef82b8315439ceb24746
Ioannis Georgoulas <geototti21@hotmail.com>
2019-06-23 12:06:29 +0100

    increase coverage

--- logic_test.go
+++ logic_test.go
@@ -9,3 +9,9 @@ func TestNew(t *testing.T) {
 	app := New("my-name", 3)
 	assert.Equal(t, &App{Name: "my-name", Version: 3}, app)
 }
+
+
+func TestNewV1(t *testing.T) {
+	app := NewV1("my-name")
+	assert.Equal(t, &App{Name: "my-name", Version: 1}, app)
+}
